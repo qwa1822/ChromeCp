78fc7fc2ac5aff4721630f777a067f9cc74df69d
qwa1822 <asdzxc1822@naver.com>
2023-05-18 07:42:27 +0900

    'update'

--- src/clock.js
+++ src/clock.js
@@ -10,7 +10,7 @@ const getNowDate=()=>{
   const week=['일요일','월요일','화요일','수요일','목요일','금요일','토요일']
   let month=modifiyNumber(nowDate.getMonth()+1)
   let date=modifiyNumber(nowDate.getDate());
- 
+  let day=nowDate.getDay();
 
 
   
